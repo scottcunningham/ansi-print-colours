3b5fc1cc2d65e6c6a36d56762ae8b76093f49035
Scott Cunningham <cunninsc@tcd.ie>
2013-02-11 23:42:04 +0000

    Initial commit

---
+++ ansi-colours.h
@@ -0,0 +1,16 @@
+#ifndef ANSI_COLOURS_H
+#define ANSI_COLOURS_H
+
+char * red="\e[0;31m";
+char * RED="\e[1;31m";
+char * blue="\e[0;34m";
+char * BLUE="\e[1;34m";
+char * cyan="\e[0;36m";
+char * CYAN="\e[1;36m";
+char * green="\e[0;32m";
+char * GREEN="\e[1;32m";
+char * yellow="\e[0;33m";
+char * YELLOW="\e[1;33m";
+char * NC="\e[0m";
+
+#endif
